6e9c44bf936dbae001a6192a8bca07dcd2110d62
Munhangyeol <mhg10181018@gmail.com>
2026-02-14 16:26:24 +0900

    fix: correct path for datatable module in sync-to-frontend script

--- scripts/sync-to-frontend.sh
+++ scripts/sync-to-frontend.sh
@@ -5,4 +5,4 @@ rm -rf ../package/frontend/node_modules/@vendor || true
 mkdir -p ../package/frontend/node_modules/@vendor
 
 cp -r ../package/quasar/dist ../package/frontend/node_modules/@vendor/quasar
-cp -r ../package/datatable/dist ../package/frontend/node_modules/@vendor/datatable
+cp -r ../package/dataTable/dist ../package/frontend/node_modules/@vendor/datatable
