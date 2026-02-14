a6a50df974ca6efbebe8c502c846d4774781bf9e
Munhangyeol <mhg10181018@gmail.com>
2026-02-14 16:20:01 +0900

    fix: correct paths in sync-to-frontend script and add index.html file

--- scripts/sync-to-frontend.sh
+++ scripts/sync-to-frontend.sh
@@ -4,6 +4,5 @@ set -e
 rm -rf ../package/frontend/node_modules/@vendor || true
 mkdir -p ../package/frontend/node_modules/@vendor
 
-cp -r ../packages/quasar/dist ../packages/frontend/node_modules/@vendor/quasar
-cp -r ../packages/datatable/dist ../packages/frontend/node_modules/@vendor/datatable
-  
+cp -r ../package/quasar/dist ../package/frontend/node_modules/@vendor/quasar
+cp -r ../package/datatable/dist ../package/frontend/node_modules/@vendor/datatable
